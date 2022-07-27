86d87c9401bf0e621644ef241c2359460f4275ba
wmorales <wmorales@kubixcorp.com>
2022-07-27 15:18:00 -0600

    se agregaron las  imagenes, y loaders necesarios

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
   mode: "development",
   resolve: {
     extensions: [".js", "jsx"],
+    alias: {
+      '@components': path.resolve(__dirname,'src/components/'),
+      '@containers':path.resolve(__dirname,'src/container/'),
+      '@styles':path.resolve(__dirname,'src/styles/'),
+      '@icons':path.resolve(__dirname,'src/assets/icons/'),
+      '@logos':path.resolve(__dirname,'src/assets/logos/'),
+    }
   },
   module: {
     rules: [
@@ -33,6 +40,10 @@ module.exports = {
         test: /\.(css|scss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset'
+      }
     ],
   },
   plugins: [
